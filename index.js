33f553fa14e5ba47a6dbc06ba909a34c1dcb2030
AbduhamidovAbduqodir <abduhamidov20099@gmail.com>
2024-06-04 12:31:27 +0500

    test3

---
+++ index.js
@@ -0,0 +1,45 @@
+let a = prompt("yoshigiz nechida ?")
+
+if (0 >= a) {
+    console.log("Men 0 yoshda man");
+}
+
+else if (1 >= a) {
+    console.log("Men 1 yoshda man");
+}
+
+else if (2 >= a) {
+    console.log("Men 2 yoshda man");
+}
+
+else if (3 >= a) {
+    console.log("Men 3 yoshda man");
+}
+
+else if (4 >= a) {
+    console.log("Men 4 yoshda man");
+}
+
+else if (5 >= a) {
+    console.log("Men 5 yoshda man");
+}
+
+else if (6 >= a) {
+    console.log("Men 6 yoshda man");
+}
+
+else if (7 >= a) {
+    console.log("Men 7 yoshda man");
+}
+
+else if (8 >= a) {
+    console.log("Men 8 yoshda man");
+}
+
+else if (9 >= a) {
+    console.log("Men 9 yoshda man");
+}
+
+else {
+    console.log("xato");
+}
